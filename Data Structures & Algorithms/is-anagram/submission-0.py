5b3b08b684ce44d9337f5854adde93ec04bbde5e
Blitzen917 <36349453+Blitzen917@users.noreply.github.com>
2026-05-14 14:38:57 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        shash = {}
+        thash = {}
+        for letter in s:
+            if letter not in shash:
+                shash[letter]=1
+            else:
+                shash[letter]+=1
+        for letter in t:
+            if letter not in thash:
+                thash[letter]=1
+            else:
+                thash[letter]+=1
+        if shash.items() == thash.items():
+            return True
+        else:
+            return False
+        
